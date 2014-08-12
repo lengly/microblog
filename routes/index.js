49590e74a89260e6589597aea7f2a141133a7722
lengly <liu_yuda@163.com>
2014-08-12 18:14:13 +0800

    add posts, FINISHED

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,16 @@
  */
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		res.render('index', { title: '首页', layout: 'layout'});
+		Post.get(null, function(err, posts) {
+			if (err) {
+				posts = [];
+			}
+			res.render('index', { title: '首页', posts: posts, layout: 'layout'});
+		});
 	});
 
 	app.get('/reg', checkNotLogin);
@@ -86,6 +92,36 @@ module.exports = function(app) {
 		req.flash('success', '登出成功');
 		res.redirect('/');
 	});
+
+	app.post('/post', checkLogin);
+	app.post('/post', function(req, res) {
+		var currentUser = req.session.user;
+		var post = new Post(currentUser.name, req.body.post);
+		post.save(function(err) {
+			if (err) {
+				req.fash('error', err);
+				return res.redirect('/');
+			}
+			req.flash('success', '发表成功');
+			res.redirect('/u/' + currentUser.name);
+		});
+	});
+
+	app.get('/u/:user', function(req, res) {
+		User.get(req.params.user, function(err, user) {
+			if (!user) {
+				req.flash('error', '用户不存在');
+				return res.redirect('/');
+			}
+			Post.get(user.name, function(err, posts) {
+				if (err) {
+					req.flash('error', err);
+					return res.redirect('/');
+				}
+				res.render('user', { title:user.name, posts:posts,})
+			})
+		})
+	})
 };
 
 function checkLogin(req, res, next) {
